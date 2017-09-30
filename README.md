6ac05c70fe26dad57f620a362c0233e2fe3860f6
andrius.kripaitis <andrius.kripaitis@gmail.com>
2017-09-30 17:12:18 +0300

    Problem description and simple solution added;

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # masked-input-focus
+
+Problem - we have a masked input (input element which is covered with div). We want to make sure that input cursor is placed into correct location when unmasking element.
+
+30 min solution which seems to be working on Chrome and IE11 is here.
---
+++ index.js
@@ -0,0 +1,50 @@
+function init() {
+    const container = document.getElementById('component');
+    const testElement = document.createElement('div');
+    testElement.style.visibility = 'hidden';
+    testElement.style.height = 'auto';
+    testElement.style.width = 'auto';
+    testElement.style.whiteSpace = 'nowrap';
+    testElement.style.position = 'absolute';
+    container.appendChild(testElement);
+
+    const mask = document.getElementById('mask');
+    mask.style.position = 'absolute';
+    mask.style.fontSize = '1em';
+    mask.addEventListener('click', function(e) {
+        mask.style.display = 'none';
+        input.style.display = 'block';
+        const position = getPositionInText(e.clientX, input.value);
+        input.setSelectionRange(position, position);
+        input.focus();
+    });
+
+    const input = document.getElementById('input');
+    input.style.position = 'absolute';
+    input.style.fontSize = '1em';
+    input.style.padding = 0;
+    input.style.border = 0;
+    input.addEventListener('blur', hideEditor);
+
+    function hideEditor() {
+        input.style.display = 'none';
+        mask.style.display = 'block';
+        mask.innerText = input.value;
+    }
+
+    function getPositionInText(position, text) {
+        for(let i = 0; i < text.length; i += 1) {
+            testElement.innerText = text.substring(0, i);
+            if (testElement.clientWidth > position) {
+                return i-1;
+            }
+        }
+        return text.length;
+    }
+
+    hideEditor();
+}
+
+window.addEventListener('load', function() {
+    init();
+});
